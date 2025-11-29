313e01536e1b3cc3d54bc0b6fb7f05dd28dcffc9
jburleigh92 <74757858+jburleigh92@users.noreply.github.com>
2025-11-28 21:15:54 -0800

    Update __init__.py

--- src/postpay/parsers/__init__.py
+++ src/postpay/parsers/__init__.py
@@ -1,13 +1,19 @@
-from .apple_parser import ApplePayParser
-from .cashapp_parser import CashAppParser
-from .other_parsers import OtherPaymentParser
-from .venmo_parser import VenmoParser
+"""
+Payment Parser Registry
+
+Exports all supported payment provider parsers.
+"""
+
 from .zelle_parser import ZelleParser
+from .venmo_parser import VenmoParser
+from .cashapp_parser import CashAppParser
+from .apple_parser import ApplePayParser
+from .other_parsers import OtherPaymentParser
 
 __all__ = [
-    "ApplePayParser",
-    "CashAppParser",
-    "OtherPaymentParser",
-    "VenmoParser",
     "ZelleParser",
+    "VenmoParser",
+    "CashAppParser",
+    "ApplePayParser",
+    "OtherPaymentParser",
 ]
